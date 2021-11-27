937895e1516d48924ea5faf74960c1883e3df1c9
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-27 14:07:48 +0700

    added username as response

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -8,15 +8,16 @@ class Auth_model extends CI_Model
 
         $password = $this->db->escape(md5($password));
 
-        $user_id = $this->db->query('SELECT user_id FROM user WHERE username='. $email. ' AND password=' .$password);
+        $user_id = $this->db->query('SELECT username, user_id FROM user WHERE username='. $email. ' AND password=' .$password);
 
         $result = $user_id->result_array();
 
-        if (count($result) == 1){
+        if (count($result) > 0){
             $tokenData = array();
             $tokenData['id'] = $email . $password;
 
             $output[0]['token'] = AUTHORIZATION::generateToken($tokenData);
+            $output[0]['username'] = $result[0]['username'];
             $output[0]['user_id'] = $result[0]['user_id'];
 
             return $output;
